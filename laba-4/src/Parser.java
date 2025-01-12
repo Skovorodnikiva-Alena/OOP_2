dd36ad26c258dd1be4bc8da2df33839355f777c5
Skovorodnikova-Alena <alenaalena222000@gmail.com>
2025-01-12 13:14:56 +0300

    add

--- laba-4/src/Parser.java
+++ laba-4/src/Parser.java
@@ -1,16 +1,17 @@
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
 
-class Parser {
-    public List<String> parse(String path) {
+public class Parser {
+
+    public List<String> parse(String path) throws IOException {
         try {
             return Files.readAllLines(Paths.get(path));
-        } catch (IOException e) {
-            throw new IllegalArgumentException("Файл " + path + " не найден");
+        } catch (NoSuchFileException e) {
+            throw new NoSuchFileException("Файл " + path + " не найден");
         }
     }
 }
